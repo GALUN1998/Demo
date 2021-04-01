e751ef46a51d60df6373fa195097931d0f028c8c
YangJiaLun <galun1998@163.com>
2021-04-01 22:04:11 +0800

    the usage of git

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main()
 {
     cout << "Hello git !" << endl;
+    for(int i = 0; i < 10; ++i) 
+    {
+        if(i == 5) 
+        {
+            break;
+        }
+    }
+    return 0;
 }
